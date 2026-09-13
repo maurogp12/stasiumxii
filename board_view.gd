1560e1dbadbae2688a7a16ef4f8934864ec27887
JavierP <silver9ga@gmail.com>
2026-09-12 23:49:08 -0500

    end turn, check for collision vs players, and refined movement

--- board_view.gd
+++ board_view.gd
@@ -8,7 +8,10 @@ const MAX_MP: int = 3
 
 var tiles: Dictionary = {}
 var selected_tile: BoardTile = null
+
+var pawns: Array[Pawn] = []
 var pawn: Pawn
+var active_pawn_index: int = 0
 var remaining_mp: int = MAX_MP
 
 func _ready() -> void:
@@ -19,15 +22,18 @@ func _ready() -> void:
 			tile.grid_position = Vector2i(x, y)
 
 			tile.position = Vector2(
-			(x - y) * 32,
-			(x + y) * 16
+				(x - y) * 32,
+				(x + y) * 16
 			)
 
 			$Tiles.add_child(tile)
 			tiles[tile.grid_position] = tile
-			print(tile.grid_position)
-	spawn_pawn()
-	end_turn()
+	pawn = spawn_pawn(Vector2i(6,6))
+	var pawn_two = spawn_pawn(Vector2i(1,1))
+	pawns.append(pawn)
+	pawns.append(pawn_two)
+	
+	
 
 func local_to_grid(point: Vector2) -> Vector2i:
 	var grid_x := point.x / 64.0 + point.y / 32.0
@@ -41,7 +47,7 @@ func local_to_grid(point: Vector2) -> Vector2i:
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var mouse_position = $Tiles.get_local_mouse_position()
+			var mouse_position: Vector2 = $Tiles.get_local_mouse_position()
 			var cell := local_to_grid(mouse_position)
 
 			if cell.x >= 0 and cell.x < BOARD_SIZE:
@@ -56,16 +62,17 @@ func select_tile(cell: Vector2i) -> void:
 	selected_tile = tiles[cell] as BoardTile
 	selected_tile.set_selected(true)
 
-func spawn_pawn() -> void:
-	pawn = PAWN_SCENE.instantiate() as Pawn
-	pawn.grid_position = Vector2i(1, 1)
+func spawn_pawn(start_cell: Vector2i) -> Pawn:
+	var new_pawn = PAWN_SCENE.instantiate() as Pawn
+	new_pawn.grid_position = start_cell
 
-	pawn.position = Vector2(
-		(pawn.grid_position.x - pawn.grid_position.y) * 32,
-		(pawn.grid_position.x + pawn.grid_position.y) * 16
+	new_pawn.position = Vector2(
+		(new_pawn.grid_position.x - new_pawn.grid_position.y) * 32,
+		(new_pawn.grid_position.x + new_pawn.grid_position.y) * 16
 	)
 
-	$Units.add_child(pawn)
+	$Units.add_child(new_pawn)
+	return new_pawn
 
 func try_move_pawn(destination: Vector2i) -> void:
 	if remaining_mp <= 0:
@@ -77,7 +84,10 @@ func try_move_pawn(destination: Vector2i) -> void:
 
 	if distance != 1:
 		return
-
+	
+	if is_tile_occupied(destination):
+		return
+	
 	pawn.grid_position = destination
 	remaining_mp -= 1
 
@@ -87,7 +97,22 @@ func try_move_pawn(destination: Vector2i) -> void:
 	)
 
 	print("MP remaining: ", remaining_mp)
-	
+
+func is_tile_occupied(cell: Vector2i) -> bool:
+	for unit in pawns:
+		if unit.grid_position == cell:
+			return true
+	return false
+
 func end_turn() -> void:
+	if active_pawn_index == 0:
+		active_pawn_index = 1
+	elif active_pawn_index == 1:
+		active_pawn_index = 0
 	remaining_mp = MAX_MP
+	pawn = pawns[active_pawn_index]
 	print("MP remaining: ", remaining_mp)
+
+
+func _on_end_turn_button_pressed() -> void:
+	end_turn()
